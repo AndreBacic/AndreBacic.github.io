c96dcf3d9d0217ce3ebc1f813234da73e783d674
andre9050@yahoo.com <andre9050@yahoo.com>
2022-03-04 08:57:46 -0500

    security measures

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g
+
+function isValidEmail(email) {
+    regexed = email.match(emailRegex)
+    if (!regexed) {return false}
+    if (regexed.length > 1) {return false}
+    if (regexed[0] !== email) {return false}
+    
+    return true
+}
+
+isMoreRecentDate = (d1, d2) => {return Date.parse(d1) < Date.parse(d2)}
 
 const form = document.getElementById("form")
 form.addEventListener("submit", (event) => {
@@ -10,7 +22,7 @@ form.addEventListener("submit", (event) => {
     }
 
     if ( templateParams.from_name == '' || 
-        !templateParams.from_email.match(/[@]/g) ||
+        !isValidEmail(templateParams.from_email) ||
         templateParams.message == '') {
             window.alert("Please fill in all fields before sending a message.")
             return
@@ -27,6 +39,21 @@ form.addEventListener("submit", (event) => {
     form.from_email.value = ""
     form.message.value = ""
 })
+
+const SENT_EMAIL_RECORDS_NAME = "SENT_EMAIL_RECORDS"
+const MAX_EMAILS_PER_DAY = 4
+function numSentFormEmails() {
+    // get records from LS
+    // delete expired records
+    // return tally
+}
+function saveSentEmailRecord() {
+    let now = new Date().toString()
+    let records = JSON.parse(localStorage.getItem(SENT_EMAIL_RECORDS_NAME))
+    records.push(now)
+    localStorage.setItem(SENT_EMAIL_RECORDS_NAME, records)
+}
+
 
 let link = document.querySelector("link[rel~='icon']");
 let lastBodyWidth = 1000 // assumes laptop
